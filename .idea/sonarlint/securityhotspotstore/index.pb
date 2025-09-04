2f7a45f1548be3c3845c4329d15ba2bd48659a64
Madrea <madrea.mircea99@gmail.com>
2025-09-05 00:13:19 +0300

    push

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+index.html,f\6\f6013a00b362253c64368d6eebc50ea2131754e2
+K
+.well-known/assetlinks.json,d\0\d009fad5513399743a8ca9c424dd8cc1038407d9
---
+++ well-known/assetlinks.json.php
@@ -0,0 +1,12 @@
+[
+    {
+        "relation": ["delegate_permission/common.handle_all_urls"],
+        "target": {
+            "namespace": "android_app",
+            "package_name": "com.madreamircea.fridgy",
+            "sha256_cert_fingerprints": [
+            "42:88:1E:DE:E4:00:AF:2C:46:8A:E7:6C:B4:6A:6F:AF:EE:DE:22:98:EE:AE:A4:99:19:0D:EA:8B:8A:30:E2:78"
+            ]
+        }
+    }
+]
